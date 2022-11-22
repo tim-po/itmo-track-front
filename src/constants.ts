8e321d31966904b990328dd7c6fa43d8a8bc8ea6
Petr.Bur <gtnh48965@bk.ru>
2022-11-22 13:48:05 +0300

    fix style

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ export const COLORS_BY_CATEGORY: {[key: string]: string} = {
   'Art & Science': 'var(--color-4-light)',
 }
 
+
 export const colors: { [key: string]: string } = {
   "программирование и информационные технологии": "#7BA3E7",
   "soft-skills": "#F49987",
